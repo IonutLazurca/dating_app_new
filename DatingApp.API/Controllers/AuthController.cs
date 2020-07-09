9b4a5b007c47c5f32bf6ae97b1227772fe0c08cb
Ionut Lazurca <ionut.lazurca@yahoo.com>
2020-07-09 18:27:18 +0300

    Handle errors

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -50,6 +50,9 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
+
+            //throw new Exception("Computer says no");
+
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
@@ -77,10 +80,10 @@ namespace DatingApp.API.Controllers
 
             var token = tokenHandler.CreateToken(tokenDescriptor);
 
-            return Ok(new 
+            return Ok(new
             {
                 token = tokenHandler.WriteToken(token)
-            });  
+            });
         }
 
     }
